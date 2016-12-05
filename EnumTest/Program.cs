763437ab982ff9491850f2b74d5db6b46acafe7d
Ted Burhan <ted.burhan@wisetechglobal.com>
2016-12-06 09:25:14 +1100

    test123

---
+++ EnumTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var fields = typeof(Operations)
+				.GetFields(BindingFlags.Public | BindingFlags.Static)
+				.Where(f => f.GetCustomAttribute<MyAttribute>() != null)
+				.ToArray();
+
+			Enum x = Operations.IsBlank;
+			var y = fields[0].GetValue(null);
+
+			var xArr = new[] { x };
+			var yArr = new[] { y };
+			var zArr = xArr.Where(a => yArr.Contains(a));
+			Console.WriteLine(fields.Count());
+			Console.Read();
+		}
+	}
+
+	public enum Operations
+	{
+		StartsWith,
+		Is,
+		[My]
+		IsBlank,
+		IsNot,
+		[My]
+		IsNotBlank
+	};
+
+	public class MyAttribute : Attribute
+	{
+	}
+
+}
